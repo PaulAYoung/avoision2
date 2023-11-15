5cd109e3cfb641ad3b6c1bafb7aade6976af9257
Paul Young <paulandresyoung@gmail.com>
2023-11-14 21:53:26 -0800

    game start with space

--- src/menu.rs
+++ src/menu.rs
@@ -3,21 +3,30 @@ use crate::GameState;
 
 pub struct Menu;
 
-pub struct Menu_Text;
+#[derive(Component)]
+pub struct MenuEntity;
 
 impl Plugin for Menu {
     fn build(&self, app: &mut App) {
         app.add_systems(
             OnEnter(GameState::Menu),
             (
-                spawn_menu_text,   
+                spawn_menu_text,
             )
-        );
+        )
+        .add_systems(Update, (
+            start_game_on_space
+        ).run_if(in_state(GameState::Menu)))
+        .add_systems(OnExit(GameState::Menu),
+            exit_menu
+        )
+        ;
     }
 }
 
-pub fn spawn_menu_text(mut commands: Commands){
+fn spawn_menu_text(mut commands: Commands){
     commands.spawn((
+        MenuEntity,
         // Create a TextBundle that has a Text with a single section.
         TextBundle::from_section(
             // Accepts a `String` or any type that converts into a `String`, such as `&str`
@@ -29,4 +38,22 @@ pub fn spawn_menu_text(mut commands: Commands){
             },
         )
     ));
+}
+
+fn start_game_on_space(
+    keys: Res<Input<KeyCode>>,
+    mut next_state: ResMut<NextState<GameState>>
+){
+    if keys.pressed(KeyCode::Space){
+        next_state.set(GameState::InGame);
+    }
+}
+
+fn exit_menu(
+    mut commands: Commands,
+    query: Query<Entity, With<MenuEntity>>
+){
+    for e in query.iter(){
+        commands.entity(e).despawn_recursive();
+    }
 }
