8fbcd5bbabc22e591a1b9a9adeef12e74228b50e
Paul Young <paulandresyoung@gmail.com>
2023-11-15 22:13:07 -0800

    count score

--- src/in_game.rs
+++ src/in_game.rs
@@ -1,18 +1,26 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, time::Stopwatch};
 
 use crate::{bundles::{self, PhysEntity}, components::{Momentum, Collider}, systems::physics::collisions};
 use crate::GameState;
 use crate::systems;
 use crate::constants::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
 pub struct InGame;
+
+#[derive(Resource)]
+pub struct Score(Stopwatch);
 
 impl Plugin for InGame {
     fn build(&self, app: &mut App) {
-        app.add_systems(
+        app
+        .insert_resource(Score(Stopwatch::new()))
+        .add_systems(
             OnEnter(GameState::InGame),
             (
                 spawn_avoidee,
                 spawn_avoider,
+                spawn_score_text,
+                rest_score,
             )
         )
         .add_systems(
@@ -22,6 +30,7 @@ impl Plugin for InGame {
                 systems::controls::player_controls,
                 loop_space,
                 collisions,
+                update_score,
             ).run_if(in_state(GameState::InGame))
         );
     }
@@ -83,4 +92,38 @@ fn loop_space(
         if t.translation.x < -SCREEN_WIDTH/2. {t.translation.x = SCREEN_WIDTH/2.;}
         if t.translation.x > SCREEN_WIDTH/2. {t.translation.x = -SCREEN_WIDTH/2.;}
     }
+}
+
+fn rest_score(mut score: ResMut<Score>){
+    score.0.reset();
+    score.0.unpause();
+}
+
+#[derive(Component)]
+pub struct ScoreText;
+
+fn spawn_score_text(mut commands: Commands){
+    commands.spawn((
+        ScoreText,
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "Start: 0",
+            TextStyle {
+                // This font is loaded and will be used instead of the default font.
+                font_size: 12.0,
+                ..default()
+            },
+        )
+    ));
+}
+
+fn update_score(
+    mut score: ResMut<Score>,
+    time: Res<Time>,
+    mut query: Query<&mut Text, With<ScoreText>>
+){
+    score.0.tick(time.delta());
+    let mut score_text = query.get_single_mut().expect("Did not find score text.");
+    score_text.sections[0].value = format!("Score: {}", score.0.elapsed().as_secs().to_string());
 }
